96224e47a17774f54b01b3d1a9c184ecc878514e
IKY <rizkykucuk19@gmail.com>
2026-08-19 00:52:53 +0700

    config.js

---
+++ js/config.js
@@ -0,0 +1,30 @@
+/**
+ * CONFIGURATION
+ * Ubah API_URL dengan URL Google Apps Script Web App Anda
+ */
+
+const CONFIG = {
+    // Ganti dengan URL Web App Anda setelah deploy
+    API_URL: 'https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec',
+    
+    // Interval polling dalam milidetik
+    POLLING_INTERVAL: 5000, // 5 detik
+    
+    // Interval heartbeat
+    HEARTBEAT_INTERVAL: 10000, // 10 detik
+    
+    // Timeout offline (detik)
+    OFFLINE_TIMEOUT: 30,
+    
+    // Items per page
+    ITEMS_PER_PAGE: 20,
+    
+    // Versi
+    VERSION: '1.0.0'
+};
+
+// Storage keys
+const STORAGE_KEYS = {
+    TOKEN: 'authToken',
+    USER: 'userData'
+};
