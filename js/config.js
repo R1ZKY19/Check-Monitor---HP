daa406fb53e7a9a0551b8a99870dc1b16d2ec77b
IKY <rizkykucuk19@gmail.com>
2026-08-19 02:49:30 +0700

    Improve dashboard polling and persistence

--- js/config.js
+++ js/config.js
@@ -1,29 +1,16 @@
 /**
  * CONFIGURATION
- * Ubah API_URL dengan URL Google Apps Script Web App Anda
+ * Check Monitor
  */
-
 const CONFIG = {
-    // URL Web App Google Apps Script terbaru
     API_URL: 'https://script.google.com/macros/s/AKfycbyQuN32b1_u-JB5I38Da9spv7oCHTRgUh8_6s004CD1rrmcC23aanpEn90ScwS0IyUS/exec',
-    
-    // Interval polling dalam milidetik
-    POLLING_INTERVAL: 5000,
-    
-    // Interval heartbeat
-    HEARTBEAT_INTERVAL: 10000,
-    
-    // Timeout offline (detik)
-    OFFLINE_TIMEOUT: 30,
-    
-    // Items per page
+    POLLING_INTERVAL: 15000,
+    HEARTBEAT_INTERVAL: 30000,
+    OFFLINE_TIMEOUT: 60,
     ITEMS_PER_PAGE: 20,
-    
-    // Versi
-    VERSION: '1.0.0'
+    VERSION: '3.0.0'
 };
 
-// Storage keys
 const STORAGE_KEYS = {
     TOKEN: 'authToken',
     USER: 'userData'
